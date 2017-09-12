8a2694d34b4255bc53a4b50d28a27745ecd46343
Felipe Véras <lfvg@cin.ufpe.br>
2017-09-12 14:07:51 -0300

    Edição do nome do pacote de 'Crawler' para 'crawler'

---
+++ src/crawler/Link.java
@@ -0,0 +1,17 @@
+package crawler;
+
+public class Link {
+	private int peso;
+	private String url;
+	public Link(int peso, String url){
+		this.peso=peso;
+		this.url=url;
+	}
+	public int getPeso() {
+		return peso;
+	}
+	public String getUrl() {
+		return url;
+	}
+	
+}
