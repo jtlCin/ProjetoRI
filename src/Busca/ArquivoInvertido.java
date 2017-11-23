43d46a4b89eea9660361600ea4c59003054ad02b
Felipe Véras <lfvg@cin.ufpe.br>
2017-11-23 12:09:46 -0300

    .
    .

---
+++ src/Busca/ArquivoInvertido.java
@@ -0,0 +1,166 @@
+package Busca;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Scanner;
+
+public class ArquivoInvertido {
+	private static Hashtable arquivo;
+
+	public ArquivoInvertido() {
+
+	}
+
+	public ArquivoInvertido(boolean select) {
+		if (!select) {
+			// tenta ler o arquivo de lista invertida
+			try {
+				File arquivoInvertido = new File("ai");
+				Scanner sc = new Scanner(arquivoInvertido);
+				// ler enquanto tiver linha (cada linha representa uma lista de
+				// documentos/frequancia separados por espaço que contem o termo
+				// que
+				// aparece como 1 elemento da linha)
+				while (sc.hasNext()) {
+					// vai salvar o ultimo node inserido para facilitar a
+					// insercao
+					// de novos nodes
+					Node nodeTemp;
+					// ler a linha
+					String temp = sc.nextLine();
+					// splita nos espacos
+					String[] arrayTemp = temp.split(" ");
+					// cria o head da lista, assume que se o termo esta no
+					// documento
+					// ele aparece em pelo menos uma pagina com uma frequancia,
+					// entao podemos pegar 3 elementos
+					// no arrayTemp: arrayTemp[0] = String do termo,
+					// arrayTemp[1] =
+					// primeiro documento em que o termo aparece, arrayTemp[2] =
+					// frequencia do termo nesse documento
+					nodeTemp = new Node(arrayTemp[0], Integer.parseInt(arrayTemp[1]), Integer.parseInt(arrayTemp[2]));
+					// insere o head na hastable
+					arquivo.put(arrayTemp[0], nodeTemp);
+					// para cada um dos outros pares documento/frequencia,
+					// insere
+					// eles no next do ultimo node inserido e seta o novo
+					// nodeTemp
+					for (int i = 3; i < arrayTemp.length; i += 2) {
+						nodeTemp.setNext(new Node(Integer.parseInt(arrayTemp[i]), Integer.parseInt(arrayTemp[i + 1])));
+						nodeTemp = nodeTemp.getNext();
+					}
+				}
+				// caso o arquivo nao exista busca criar o arquivo
+			} catch (FileNotFoundException e) {
+				String listaArquivos[];
+				// substituir a pasta paginas
+				listaArquivos = new File("Teste").list();
+				try {
+					bancoPalavras(listaArquivos);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		}
+		else{
+			try {
+				File arquivoInvertido = new File("aic");
+				File arquivoInvertidoHead = new File("haic");
+				FileInputStream sc = new FileInputStream("aic");
+				Scanner hsc = new Scanner(arquivoInvertidoHead);
+				String temp[] =  hsc.nextLine().split(" ");
+				String key = temp[0];
+				int qt =  Integer.parseInt(temp[1]);
+				byte tempNumDoc = (byte) sc.read();
+				byte tempFreq = (byte) sc.read();
+				Node nodeTemp = new Node(tempNumDoc, tempFreq);
+				arquivo.put(key, nodeTemp);
+				while (qt>1){
+					// vai salvar o ultimo node inserido para facilitar a
+					// insercao
+					// de novos nodes
+					// ler a linha
+					tempNumDoc = (byte) sc.read();
+					// splita nos espacos
+					tempFreq = (byte) sc.read();
+					// cria o head da lista, assume que se o termo esta no
+					// documento
+					// ele aparece em pelo menos uma pagina com uma frequancia,
+					// entao podemos pegar 3 elementos
+					// no arrayTemp: arrayTemp[0] = String do termo,
+					// arrayTemp[1] =
+					// primeiro documento em que o termo aparece, arrayTemp[2] =
+					// frequencia do termo nesse documento
+					nodeTemp.setNext(new Node(tempNumDoc, tempFreq));
+					nodeTemp = nodeTemp.getNext();	
+				}
+				// caso o arquivo nao exista busca criar o arquivo
+			} catch (IOException e) {
+				
+				//
+				//
+				//
+				//
+				//
+				//
+				//
+				//
+				//
+				//
+				String listaArquivos[];
+				// substituir a pasta paginas
+				listaArquivos = new File("Teste").list();
+				try {
+					bancoPalavras(listaArquivos);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public static void bancoPalavras(String[] listaArquivos) throws FileNotFoundException {
+		for (int i = 1; i <= listaArquivos.length; i++) {
+			Hashtable tempHash = new Hashtable();
+			String st = "";
+			String arrayTemp[];
+			File temp = new File("Teste\\" + i + ".txt");
+			Scanner tempScan = new Scanner(temp);
+			while (tempScan.hasNext()) {
+				st += tempScan.nextLine() + " ";
+			}
+			arrayTemp = st.split(" ");
+			for (int ii = 0; ii < arrayTemp.length; ii++) {
+				if (tempHash.containsKey(arrayTemp[ii])) {
+					int aux = (int) tempHash.get(arrayTemp[ii]);
+					tempHash.put(arrayTemp[ii], aux + 1);
+				} else
+					tempHash.put(arrayTemp[ii], 1);
+			}
+			int s = 0;
+			Enumeration names = tempHash.keys();
+			while (s < tempHash.size()) {
+				String key = (String) names.nextElement();
+				int k = (int) tempHash.get(key);
+				if (arquivo.containsKey(key)) {
+					Node tempNode = (Node) arquivo.get(key);
+					tempNode.setNext(new Node(i, k));
+					arquivo.put(key, tempNode);
+				} else
+					arquivo.put(key, new Node(key, i, k));
+				s++;
+			}
+		}
+	}
+
+	public Hashtable getArquivoInvertido() {
+		return this.arquivo;
+	}
+
+}
---
+++ src/Busca/Node.java
@@ -0,0 +1,58 @@
+package Busca;
+
+public class Node {
+	String nome;
+	private int numDoc;
+	private int freq;
+	private Node next;
+	private byte freqByte;
+	private byte numDocByte;
+	 
+	public Node (int num, int freq){
+		this.nome = null;
+		this.numDoc = num;
+		this.freq = freq;
+		next = null;
+	}
+	public Node (String nome, int num, int freq){
+		this.nome = nome;
+		this.numDoc = num;
+		this.freq = freq;
+		next = null;
+	}
+	public Node (byte num, byte freq){
+		this.freqByte = freq;
+		this.numDocByte = num;
+		next = null;
+	}
+	
+	public int getNumDoc() {
+		return this.numDoc;
+	}
+
+	public int getFreq() {
+		return this.freq;
+	}
+
+	public Node getNext() {
+		return this.next;
+	}
+	
+	public byte getFreqByte(){
+		return this.freqByte;
+	}
+	
+	public byte getNumDocByte(){
+		return this.numDocByte;
+	}
+	
+	public boolean hasNext(){
+		if(next!=null)return true;
+		return false;
+	}
+	
+	public void setNext(Node n){
+		if(this.next==null) this.next = n;
+		else this.next.setNext(n);
+	}
+}
