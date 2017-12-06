59b6796ef6beba2bba51e4b527a179ee5ded6dd4
WisON27 <vrm@cin.ufpe.br>
2017-12-06 01:46:57 -0300

    Implementando busca por ranking (tf aparentemente pronto e tf-idf quase).

--- src/busca/Ranking.java
+++ src/busca/Ranking.java
@@ -2,47 +2,109 @@ package busca;
 
 import java.util.Hashtable;
 import java.util.List;
+import java.util.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Ranking {
     
     private ArquivoInvertido a;
-    Hashtable indiceInvertido;
+    private Hashtable indiceInvertido;
 
     public Ranking(ArquivoInvertido a) {
         this.a = a;
-        indiceInvertido = a.getIndiceInvertido();
+        this.indiceInvertido = a.getIndiceInvertido();
     }
 
     public List search(String query) {
         /** Dado um query retorna as paginas que devem aparecer ordenadas pelos seus rankings */
-        List<String> r = new ArrayList<String>();
+        List r = new ArrayList<Pair>();
         String[] words = query.split(" ");
-        for (String word : words) {
-            if (indiceInvertido.containsKey(word)) {
+        Hashtable scoreByDocs = new Hashtable<String, Hashtable>();
+        Hashtable docs = new Hashtable(); //todos numeros de docs envolvidos
+        for (String word : words) { //organizando palavras e vetores 
+            if (indiceInvertido.containsKey(word)) { //se alguem tem a palavra
                 Node no = (Node) indiceInvertido.get(word);
+                int N = 0;
+                int Ni = 0;
                 do {
-                    String num = no.getNumDoc();
-                    int freq = no.getFreq();
-                    //em construcao
-                    r.add(num + " " + freq);
+                    N++;
+                    String doc = no.getNumDoc();
+                    docs.put(doc, ""); //salvando nome de doc
+                    double tf = no.getFreq();
+                    Ni += tf;
+                    Hashtable v;
+                    if (scoreByDocs.containsKey(doc)) {
+                        v = (Hashtable) scoreByDocs.get(doc);
+                    } else {
+                        v = new Hashtable<String, Double>();
+                    }
+                    v.put(word, tf); //o documento doc tem determinado score para essa word 
+                    scoreByDocs.put(doc, v); //guarda hashtable de doc
                     no = no.getNext();
                 } while (no != null);
             }
         }
+        Set<String> keys = docs.keySet();
+        for (String doc : keys) { //calculando similaridade
+            //dado um 'doc', fica com um hashtable que dado  uma 'word' (Se existir) diz um score que eh Wij
+            Hashtable v = (Hashtable) scoreByDocs.get(doc);
+            double somaD = 0;
+            double somaN1 = 0;
+            double somaN2 = 0;
+            for (String word : words) {
+                double wiq = 1;
+                double score = 0;
+                if (v.containsKey(word)) {
+                    score = (double) v.get(word);
+                }
+                somaD += score * wiq;
+                somaN1 += score * score;
+                somaN2 += wiq * wiq;
+            }
+            double sim = somaD / (Math.sqrt(somaN1) * Math.sqrt(somaN2));
+            r.add(new Pair(doc, sim));
+        }
+
+        Collections.sort(r, new Comparator<Pair>() {
+            public int compare(Pair one, Pair two) {
+                if (one.value == two.value) { //deixando por ordem de numeracao dos documentos
+                    String o = ext(one.doc, "(.*?)[.]");
+                    String t = ext(two.doc, "(.*?)[.]");
+                    return Integer.parseInt(o) > Integer.parseInt(t) ? 1 : -1;
+                }
+                if (one.value > two.value) return -1;
+                return 1;
+            }
+        });
+
         return r;
     }
 
     public static void main(String[] args) {
         ArquivoInvertido a = new ArquivoInvertido(false, "../Paginas/");
         Ranking r = new Ranking(a);
-        String query = "Notebooks usados";
-        print(r.search(query));
+        String query = "notebook usados processador";
+        List<Pair> l = r.search(query);
+        for (Pair i : l) {
+            System.out.println(i);
+        }
 
     }
 
+
+    public static String ext(String page, String pat) {
+        Pattern padrao = Pattern.compile(pat);
+        Matcher matcher = padrao.matcher(page);
+        if (matcher.find()) {
+            String content = matcher.group(1);
+            return content;
+        } else {
+            return "";
+        }
+    }
 
     public static void print(Object[] a) {
         System.out.println(Arrays.toString(a));
@@ -56,4 +118,18 @@ public class Ranking {
         System.out.println(Arrays.toString(a.toArray(new String[a.size()])));
     }
 
+}
+
+class Pair {
+    public double value;
+    public String doc;
+
+    public Pair(String doc, double value) {
+        this.doc = doc;
+        this.value = value;
+    }
+
+    public String toString() {
+        return (this.doc + " " + this.value);
+    }
 }
